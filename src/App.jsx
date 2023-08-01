d0374c90d16385d09711c0c8292499e5bc4179ae
Rafael <rafaelberger.dev@gmailcom>
2023-08-01 20:57:07 -0300

    Header completo, falta apenas rotear o site

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <h1 className="">hello world</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import logo from "../assets/logovini.png";
+
+function Header() {
+  return (
+    <>
+      <nav className="flex w-screen justify-between pt-6 px-32">
+        <ul className="flex w-1/4 justify-around items-center text-xl ">
+          <li>TRABALHOS</li>
+          <li>CONTATO</li>
+          <li>SOBRE MIM</li>
+        </ul>
+        <div>
+          <img src={logo} alt="Logo" className="w-48" />
+        </div>
+      </nav>
+    </>
+  );
+}
+
+export default Header;
