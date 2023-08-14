2d93abebdea5e1a92e47bac60424c83bc19f9afc
Rafael <rafaelberger.dev@gmailcom>
2023-08-14 15:26:53 -0300

    responsivo para telas grandes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ import logo from "../assets/logovini.png";
 function Header() {
   return (
     <>
-      <nav className="flex w-screen justify-between pt-6 px-32">
-        <ul className="flex w-1/4 justify-around items-center text-xl ">
+      <nav className="flex w-screen justify-between pt-6 px-32  ">
+        <ul className="flex lg:w-2/5 2xl:w-1/4 justify-around items-center text-xl ">
           <li>TRABALHOS</li>
           <li>CONTATO</li>
           <li>SOBRE MIM</li>
